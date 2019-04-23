3f8eac8428be546ee25e39f69e7e42de4c7ecfd7
Your Name <you@example.com>
2019-04-22 23:37:16 -0300

    falta agregar metodo verificar

--- Clases/Clase-6/clase06/nexo.php
+++ Clases/Clase-6/clase06/nexo.php
@@ -10,7 +10,7 @@ switch ($op)
     $id = isset($_POST['id']) ? $_POST['id'] : NULL;
 
     $usuario= usuario::TraerUno($id);
-    if($usuario!==null)
+    if($usuario!==null || $usuario!==false)
     {
         $usuario=usuario::TraerUno($id);
         echo  $usuario->MostrarDatos();
@@ -21,7 +21,62 @@ switch ($op)
     {
         "error";
     }
-       break;
+
+
+    break;
+
+    case 'traerTodos':
+    $usuarios=[];
+
+    $usuarios=usuario::TraerTodos();
+    if($usuarios!==null && count($usuarios)!==0)
+    {
+        foreach($usuarios as $user)
+        {
+            //muestro cada usuario 1 por 1 del array
+            echo $user->MostrarDatos() . "<br>";
+        }
+    }
+
+    break;
+
+    case 'ingresarUsuario':
+    $usuario = new usuario(1,"prueba@hotmail.com","ki89","prueba","pruebita",47);
+
+    echo usuario::Ingresar($usuario);
+    
+
+    echo "ok";
+
+    break;
+
+    case 'eliminarUsuario':
+
+    $usuario = new usuario(9);
+
+    echo usuario::Eliminar($usuario);
+
+    echo "ok";
+
+    break;
+
+    case 'modificarUsuario':
+
+        $id=$_POST['id'];
+        $correo=$_POST['correo'];
+        $clave=$_POST['clave'];
+        $nombre=$_POST['nombre'];
+        $apellido=$_POST['apellido'];
+        $perfil=$_POST['perfil'];
+
+        echo usuario::Modificar($id,$correo,$clave,$nombre,$apellido,$perfil);
+
+        echo "ok";
+    
+
+
+    break;
+
     
     default:
         echo ":(";
--- Clases/Clase-6/clase06/usuario.php
+++ Clases/Clase-6/clase06/usuario.php
@@ -26,7 +26,7 @@ class usuario
 
    public function MostrarDatos()
     {
-            return $this->id." - ".$this->correo." - ".$this->clave." - ".$this->nombre . "-" . $this->apellido . "-" . $this->perfil;
+            return $this->id." - ".$this->correo." - ".$this->clave." - ".$this->nombre . " - " . $this->apellido . " - " . $this->perfil;
     }
 
     #metodo estatico
@@ -58,5 +58,95 @@ class usuario
         return $usuario;
     }
 
+    public static function TraerTodos()
+    {
+      $usuarios =[];
+
+      $objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso();
+        
+        $consulta = $objetoAccesoDato->RetornarConsulta("SELECT * FROM usuarios");
+
+       // $consulta->bindValue(':id',  $id, PDO::PARAM_INT);
+
+        $consulta->execute();
+
+        //V1:obtengo la fila especificada en la consultado , como tipo indexado o por clave
+        //utilizo un while ya que necesito varias filas y creo varios objetos
+        /*while($fila = $consulta->fetch())
+        {
+          $usuario= new usuario($fila[0],$fila[1],$fila[2],$fila[3],$fila[4],$fila[5]);
+          array_push($usuarios,$usuario);
+        }*/
+
+
+        //V2:creo objetos de tipo usuario
+        //utilizo while porque necesito varias filas y creo varios objetos
+         
+        while ($fila = $consulta->fetchObject("usuario"))
+        {
+         array_push($usuarios,$fila);
+        }
+
+
+         //V3: utilizando FETCH_INTO , debo retornar la misma consulta
+        /* $consulta->setFetchMode(PDO::FETCH_INTO, new usuario());
+         return $consulta;*/
+      
+       
+        return $usuarios;
+    }
+
+    public static function Ingresar($obj)
+    {
+    
+      $objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso();
+        
+      //no le paso el id ya que es autoincremental
+      $consulta = $objetoAccesoDato->RetornarConsulta("INSERT INTO usuarios (correo, clave, nombre, apellido, perfil) values (:correo , :clave , :nombre , :apellido , :perfil)");
+
+  
+      $consulta->bindValue(':correo', $obj->correo, PDO::PARAM_STR);
+      $consulta->bindValue(':clave', $obj->clave, PDO::PARAM_STR);
+      $consulta->bindValue(':nombre', $obj->nombre, PDO::PARAM_STR);
+      $consulta->bindValue(':apellido', $obj->apellido, PDO::PARAM_STR);
+      $consulta->bindValue(':perfil', $obj->perfil, PDO::PARAM_INT);
+
+     return $consulta->execute();
+
+    }
+
+
+    public static function Eliminar($obj)
+    {
+      
+      $objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso();
+        
+      //no le paso el id ya que es autoincremental
+      $consulta = $objetoAccesoDato->RetornarConsulta("DELETE FROM usuarios where id = :id");
+
+      $consulta->bindValue(':id',$obj->id,PDO::PARAM_INT);
+
+      return $consulta->execute();
+    }
+
+    public static function Modificar($id,$correo,$clave,$nombre,$apellido,$perfil)
+    {
+
+      $objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso();
+        
+      $consulta =$objetoAccesoDato->RetornarConsulta("UPDATE usuarios SET correo = :correo, clave = :clave, 
+                                                      nombre = :nombre, apellido = :apellido, perfil = :perfil WHERE id = :id");
+      
+      $consulta->bindValue(':id',$id,PDO::PARAM_INT);
+      $consulta->bindValue(':correo', $correo, PDO::PARAM_STR);
+      $consulta->bindValue(':clave', $clave, PDO::PARAM_STR);
+      $consulta->bindValue(':nombre', $nombre, PDO::PARAM_STR);
+      $consulta->bindValue(':apellido', $apellido, PDO::PARAM_STR);
+      $consulta->bindValue(':perfil', $perfil, PDO::PARAM_INT);
+
+      return $consulta->execute();
+
+
+    }
     
 }
